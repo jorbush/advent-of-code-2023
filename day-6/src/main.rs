1b55fb898632c2f73f1ad5501254c9f9dfc5837c
Jordi <55429631+jorbush@users.noreply.github.com>
2023-12-07 08:52:52 +0100

    Day 6 uncompleted

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -46,6 +46,50 @@ fn part_one() -> io::Result<()> {
     Ok(())
 }
 
+fn part_two() -> io::Result<()> {
+    let file = File::open("input_two.txt")?;
+    let reader = io::BufReader::new(file);
+
+    let mut lines_iter = reader.lines();
+    let mut result: u32 = 1;
+
+    if let Some(Ok(line1)) = lines_iter.next() {
+        let content_time: Vec<&str> = line1.split(':').collect();
+        let input_time: Vec<&str> = content_time.get(1).unwrap().split_whitespace().collect();
+
+        println!("Times: {:?}", input_time);
+        let times: Vec<u32> = input_time.get(0).unwrap().chars().map(|c| c.to_digit(10).unwrap() as u32).collect();
+
+        if let Some(Ok(line2)) = lines_iter.next() {
+            let content_distance: Vec<&str> = line2.split(':').collect();
+            let input_distance: Vec<&str> = content_distance.get(1).unwrap().split_whitespace().collect();
+            let distances: Vec<u32> = input_distance.get(0).unwrap().chars().map(|c| c.to_digit(10).unwrap() as u32).collect();
+
+            println!("Distances: {:?}", distances);
+            let mut ways: u32 = 0;
+            for (index, &time) in times.iter().enumerate() {
+                if let Some(record_distance) = distances.get(index) {
+                    for holding_time in 14..=time - 14 {
+                        println!("Holding time {}", holding_time);
+                        let new_record = holding_time * (time - holding_time);
+                        println!("New record {}", new_record);
+                        if new_record > *record_distance {
+                            ways += 1;
+                            println!("Ways: {}", ways);
+                        }
+                    }
+                }
+                result *= ways;
+                ways = 0;
+            }
+        }
+    }
+
+    println!("Result: {}", result);
+
+    Ok(())
+}
+
 fn main() {
-    let _ = part_one();
+    let _ = part_two();
 }
